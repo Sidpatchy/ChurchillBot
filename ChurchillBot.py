b58af1803e7e457cf3130b1a33e0a409d4e82709
Rainverm38 <35241490+Rainverm38@users.noreply.github.com>
2019-04-12 19:40:51 -0500

    Update ChurchillBot.py
    Added some of the planned commands

--- ChurchillBot.py
+++ ChurchillBot.py
@@ -28,4 +28,94 @@ async def on_ready():
     print('Done Loading!')                      # Prints 'Done Loading!' in console
     print('--------------------------')
 
-    bot.run('TOKEN HERE')         # User defined bot token
+# Adds a help command that sends a message to the user rather than spamming the chat channel
+@bot.command(pass_context=True)
+async def help(ctx):
+    currentDT = DT.datetime.now()
+    author = ctx.message.author
+    embed = discord.Embed(
+        color = discord.Color.blue()
+    )
+    embed.set_author(name='Help')
+    embed.add_field(name='!info', value='Gives some information about the bot', inline=False)
+    embed.add_field(name='!canihastenk', value='Doesn\'t even consider giving the user a tenk', inline=False)
+    embed.add_field(name='!pleasecanihastenk', value='Gives the user a tenk since they asked so nicely', inline=False)
+    embed.add_field(name='!moretanks', value='Fetches more tanks for the war effort', inline=False)
+    embed.add_field(name='!democracy', value='States what Churchill thought about democracy (A message from the author: What he said is fact, anyone who says otherwise can bugger off)')
+    embed.add_field(name='fightwhere', value='Says where we will fight', inline=False)
+    await bot.send_message(author, embed=embed)
+    print('--------------------------')
+    print('Current Time:', currentDT)
+    print('Help has been run')
+    print('--------------------------')
+
+# 'Info' command
+@bot.command(pass_context=True)
+async def info(ctx):
+    currentDT = DT.datetime.now()
+    print(' ')
+    await bot.say('Rainverm38 noticed that the choices for bots dedicated to former British Prime Minister Winston Churchill was empty.') 
+    print('--------------------------')
+    print('Current Time:', currentDT)
+    print('info has been run')
+    print('--------------------------')
+
+# 'Can I has Tenk' command
+@bot.command(pass_context=True)
+async def canihastenk(ctx):
+    currentDT = DT.datetime.now()
+    await bot.say('No, say please and maybe my answer will change')
+    print('')
+    print('--------------------------')
+    print('Current Time:', currentDT)
+    print('canihastenk has been run')
+    print('--------------------------')
+
+# 'Please Can I has Tenk' command
+@bot.command(pass_context=True)
+async def pleasecanihastenk(ctx):
+    currentDT = DT.datetime.now()
+    await bot.say('Thank you for asking so nicely, yes you may has tenk.')
+    await bot.say('https://i.imgur.com/a9GfFqO.jpg')
+    print('')
+    print('--------------------------')
+    print('Current Time:', currentDT)
+    print('pleasecanihastenk has been run')
+    print('--------------------------')
+
+# 'More tanks' command
+@bot.command(pass_context=True)
+async def moretanks(ctx):
+    currentDT = DT.datetime.now()
+    await bot.say('Getting more tanks stat!')
+    await bot.say('https://media.giphy.com/media/VFZR04kiwRtbZcMjAH/giphy.gif')
+    print('')
+    print('--------------------------')
+    print('Current Time:', currentDT)
+    print('moretanks has been run')
+    print('--------------------------')
+
+# 'Democracy' command
+@bot.command(pass_context=True)
+async def democracy(ctx):
+    currentDT = DT.datetime.now()
+    await bot.say('Democracy is the worst form of government, except for all the others.')
+    print('')
+    print('--------------------------')
+    print('Current Time:', currentDT)
+    print('democracy has been run')
+    print('--------------------------')
+
+#  'Fight Where?' command
+@bot.command(pass_context=True)
+async def fightwhere(ctx):
+    currentDT = DT.datetime.now()
+    await bot.say('WE SHALL FIGHT EVERYWHERE!')
+    await bot.say('(I stole this meme from r/HistoryMemes) https://i.imgur.com/O6n5PV9.jpg')
+    print('')
+    print('--------------------------')
+    print('Current Time:', currentDT)
+    print('fightwhere has been run')
+    print('--------------------------')
+
+bot.run('TOKEN HERE')         # User defined bot token
